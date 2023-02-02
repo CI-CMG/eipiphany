3803e0af8283cda69343f276382e7c48556c1338
Chris Slater <6004777+electricsam@users.noreply.github.com>
2023-02-02 14:53:17 -0700

    fix error handler field / method name collision

--- eipiphany_core/src/eipiphany_core/framework/base/route_builder.py
+++ eipiphany_core/src/eipiphany_core/framework/base/route_builder.py
@@ -9,7 +9,7 @@ class RouteBuilder(metaclass=abc.ABCMeta):
   def __init__(self, *args, **kw):
     super().__init__(*args, **kw)
     self._routes = []
-    self._error_handler = DefaultErrorHandler()
+    self.__default_error_handler = DefaultErrorHandler()
 
   def _from(self, eip_context, endpoint_id, route_id = None):
     route = Route(eip_context, endpoint_id, route_id if route_id else str(self))
@@ -17,7 +17,7 @@ class RouteBuilder(metaclass=abc.ABCMeta):
     return route
 
   def _error_handler(self, error_handler):
-    self._error_handler = error_handler
+    self.__default_error_handler = error_handler
 
   @abc.abstractmethod
   def build(self, eip_context):
@@ -28,5 +28,5 @@ class RouteBuilder(metaclass=abc.ABCMeta):
 
   @property
   def error_handler(self):
-    return self._error_handler
+    return self.__default_error_handler
 
