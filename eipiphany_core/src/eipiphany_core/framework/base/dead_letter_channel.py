f32c6d36d6f224117d337e3f2094778a1ef6e8bc
Chris Slater <6004777+electricsam@users.noreply.github.com>
2023-02-02 14:37:25 -0700

    pass endpoint config to DLQ

--- eipiphany_core/src/eipiphany_core/framework/base/dead_letter_channel.py
+++ eipiphany_core/src/eipiphany_core/framework/base/dead_letter_channel.py
@@ -3,8 +3,9 @@ from .error_handler import ErrorHandler
 
 class DeadLetterChannel(ErrorHandler):
 
-  def __init__(self, eip_context, endpoint_id):
+  def __init__(self, eip_context, endpoint_id, endpoint_configuration = None):
     self.__endpoint = eip_context.get_endpoint(endpoint_id)
+    self.__endpoint_configuration = endpoint_configuration
     self.__prep_processor = None
 
   def on_prepare_failure(self, processor):
@@ -14,7 +15,7 @@ class DeadLetterChannel(ErrorHandler):
   def handle_exception(self, exchange):
     if self.__prep_processor:
       self.__prep_processor.process(exchange)
-    self.__endpoint.process(exchange)
+    self.__endpoint.process(exchange, self.__endpoint_configuration)
 
 
 
